ec8c3844e815c470e34273bd04213f82b477e08f
Ilya Berulava <ilyaberulava@icloud.com>
2024-07-19 12:16:54 +0300

    убрал выброс исключений

--- main.cpp
+++ main.cpp
@@ -10,36 +10,25 @@
 
 
 void print(universal_parser first);
-bool is_txt_file(const std::string& file_path) {
-    // Проверяем, что файл имеет расширение .txt
-    return file_path.size() >= 4 && file_path.substr(file_path.size() - 4) == ".txt";
-}
-
-bool is_csv_file(const std::string& file_path) {
-    // Проверяем, что файл имеет расширение .csv
-    return file_path.size() >= 4 && file_path.substr(file_path.size() - 4) == ".csv";
-}
-bool is_file(const std::string& str) {
-    for (char ch : str) {
-        if (ch == '.') {
-            return true;
-        }
-    }
-    return false;
-}
-
+bool is_txt_file(const std::string& file_path);
+bool is_csv_file(const std::string& file_path);
+bool is_file(const std::string& str);
 
 int main(int argc, char *argv[]) {
     try {
         std::cout << "Введите путь к первому файлу с расширением .txt (Выгрузка в ПК)\nпуть к файлу :";
         std::string file1;
-        std::cin >> file1;
+        std::getline(std::cin, file1);
 
-        if (!is_txt_file(file1)) {
-            std::cerr << "Ошибка: Файл должен иметь расширение .txt" << std::endl;
-            return 1;
+        // Проверка расширения файла
+        while (!is_txt_file(file1)) {
+            std::cout << "\nОшибка: Файл должен иметь расширение .txt" << std::endl;
+            std::cout << "Введите путь к первому файлу с расширением .txt (Выгрузка в ПК)\nпуть к файлу :";
+            std::getline(std::cin, file1); // Считывание строки целиком
         }
 
+        // Продолжение работы с файлом
+        std::cout << "Файл принят: " << file1 << std::endl;
         std::string source_encoding1 = detect_encoding(file1);
         std::cout << "\nОпределенная кодировка: " << source_encoding1 << std::endl;
         std::string target_encoding1 = "utf-8";
@@ -48,10 +37,13 @@ int main(int argc, char *argv[]) {
 
         std::cout << "\n\n\nВведите путь ко второму файлу с расширением .csv (Договоры на платное обучение)\nпуть к файлу :";
         std::string file2;
-        std::cin >> file2;
-        if (!is_csv_file(file2)) {
-            std::cerr << "\nОшибка: Файл должен иметь расширение .csv\n" << std::endl;
-            return 1;
+        std::getline(std::cin, file2);
+
+        // Проверка расширения файла
+        while (!is_csv_file(file2)) {
+            std::cout << "\nОшибка: Файл должен иметь расширение .csv" << std::endl;
+            std::cout << "Введите путь ко второму файлу с расширением .csv (Договоры на платное обучение)\nпуть к файлу :";
+            std::getline(std::cin, file2); // Считывание строки целиком
         }
 
         std::string source_encoding2 = detect_encoding(file2);
@@ -64,9 +56,11 @@ int main(int argc, char *argv[]) {
         universal_parser second(file2);
         std::cout <<"\nВыберите путь для выходного файла(нужно указать папку)\nпуть к папке :";
         std::string outputPath;
-        std::cin >> outputPath;
-        if (is_file(outputPath)){
-            std::cerr << "\nОшибка: Либо в пути присутствует точка, либо вы передали путь к файлу, а надо в папку .csv\n" << std::endl;
+        std::getline(std::cin, outputPath);
+        while (is_file(outputPath)){
+            std::cout << "\nОшибка: Либо в пути присутствует точка, либо вы передали путь к файлу, а надо в папку .csv\n" << std::endl;
+            std::cout <<"\nВыберите путь для выходного файла(нужно указать папку)\nпуть к папке :";
+            std::getline(std::cin, outputPath);
         }
         outputPath += "/ouput.txt";
         validation_data val(first, second, outputPath);
@@ -95,3 +89,19 @@ void print(universal_parser first){
     std::cout << c <<std::endl;
 }
 
+bool is_csv_file(const std::string& file_path) {
+    // Проверяем, что файл имеет расширение .csv
+    return file_path.size() >= 4 && file_path.substr(file_path.size() - 4) == ".csv";
+}
+bool is_file(const std::string& str) {
+    for (char ch : str) {
+        if (ch == '.') {
+            return true;
+        }
+    }
+    return false;
+}
+bool is_txt_file(const std::string& filename) {
+    // Проверяем, что файл имеет расширение .txt
+    return filename.size() > 4 && filename.substr(filename.size() - 4) == ".txt";
+}
